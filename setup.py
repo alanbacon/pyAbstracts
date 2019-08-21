64e4410910c611c8b0bb09e6f52924f8b149c8f7
Alan Bacon <alan.bacon@bbc.co.uk>
2019-08-21 15:23:31 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='pyAbstracts',
-   version='1.0.2',
+   version='1.0.3',
    description='Abstract classes that can be extended',
    author='Alan Bacon',
    author_email='alan@bacontowers.co.uk',
