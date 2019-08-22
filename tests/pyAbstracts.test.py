d7eb205ad118d7e97e4820760c98a823b26bce15
Alan Bacon <alan.bacon@bbc.co.uk>
2019-08-22 14:34:36 +0100

    add tests

---
+++ tests/pyAbstracts.test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestAbstracts(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+ 
+    def test_should_pass(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+	unittest.main()
